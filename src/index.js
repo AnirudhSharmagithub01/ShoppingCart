cf578641c2bbe5acfb9f7afacae5dfe0bdef752e
AnirudhSharmagithub01 <anirudhsharma857000@gmail.com>
2024-10-02 20:55:03 +0530

    done

--- src/index.js
+++ src/index.js
@@ -1,33 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import "./index.css";
+import { store } from "./redux/Store";
+
 import { Toaster } from "react-hot-toast";
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux'
-import rootReducer from "./Reducer/index";
-import {configureStore} from "@reduxjs/toolkit"
 
-const store = configureStore({
-  reducer:rootReducer,
-});
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <Provider store={store}>
-      <React.StrictMode>
-        <App />
-        <Toaster />
-      </React.StrictMode>
-    </Provider>
-  </BrowserRouter>
+    <BrowserRouter>
+        <Provider store={store}>
+          <App />   
+          <Toaster/>  
+        </Provider>
+    </BrowserRouter>
 
+
+
+  
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
